fcb29071beeb6f230bd6dd6bce244d247349a527
newt0 <py_lambda@yahoo.co.jp>
2020-11-26 16:43:52 +0900

    add: context API

--- src/Search.js
+++ src/Search.js
@@ -4,13 +4,23 @@ import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
 
 const Search = ({ hideButtons = false }) => {
+  // const [state, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
   const history = useHistory();
   const [input, setInput] = useState("");
 
   const search = (e) => {
     e.preventDefault();
+
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
+
     history.push("/search");
   };
 
